b7e54e75896f9abb0d34175df2a09ef6b9dc5d8a
Bas <3441183+BattleBas@users.noreply.github.com>
2020-03-11 07:57:30 -0500

    Comment on pull request

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module action-gophers
 
 go 1.14
 
-require github.com/sethvargo/go-githubactions v0.1.1
+require (
+	github.com/google/go-github/v29 v29.0.3
+	github.com/sethvargo/go-githubactions v0.1.1
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+)
--- main.go
+++ main.go
@@ -1,29 +1,61 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"os"
 
+	"golang.org/x/oauth2"
+
+	"github.com/google/go-github/v29/github"
 	"github.com/sethvargo/go-githubactions"
 )
 
+type PullRequestEvent struct {
+	PullRequest `json:"pull_request"`
+	Repository  `json:"repository"`
+}
+
+type PullRequest struct {
+	Number int
+}
+
+type Repository struct {
+	Name  string
+	Owner `json:"owner"`
+}
+
+type Owner struct {
+	Login string
+}
+
 func main() {
+
 	githubToken := githubactions.GetInput("GITHUB_TOKEN")
 	if githubToken == "" {
 		githubactions.Fatalf("missing input 'githubToken'")
 	}
 
-	event := os.Getenv("GITHUB_EVENT_PATH")
+	event, err := ioutil.ReadFile(os.Getenv("GITHUB_EVENT_PATH"))
+	if err != nil {
+		log.Printf("Can't read events: " + err.Error())
+	}
 
-	log.Printf(event)
+	var pr PullRequestEvent
+	json.Unmarshal(event, &pr)
 
-	// ctx := context.Background()
-	// ts := oauth2.StaticTokenSource(
-	// 	&oauth2.Token{AccessToken: githubToken},
-	// )
-	// tc := oauth2.NewClient(ctx, ts)
-	// client := github.NewClient(tc)
+	log.Printf("Number: %d Repo Name: %s, Owner: %s", pr.PullRequest.Number, pr.Repository.Name, pr.Owner.Login)
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
 
-	// client.Issues.CreateComment(ctx, )
+	message := "Hello World"
+	comment := github.PullRequestComment{Body: &message}
+	client.PullRequests.CreateComment(ctx, pr.Owner.Login, pr.Repository.Name, pr.Number, &comment)
 
 }
