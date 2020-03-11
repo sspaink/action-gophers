149038b0bd51103194293d007bbaf5064d644032
Bas <3441183+BattleBas@users.noreply.github.com>
2020-03-11 04:21:06 -0500

    Test getting event info

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
-import "github.com/sethvargo/go-githubactions"
+import (
+	"log"
+	"os"
+
+	"github.com/sethvargo/go-githubactions"
+)
 
 func main() {
-	fruit := githubactions.GetInput("fruit")
-	if fruit == "" {
-		githubactions.Fatalf("missing input 'fruit'")
+	githubToken := githubactions.GetInput("GITHUB_TOKEN")
+	if githubToken == "" {
+		githubactions.Fatalf("missing input 'githubToken'")
 	}
-	githubactions.AddMask(fruit)
+
+	event := os.Getenv("GITHUB_EVENT_PATH")
+
+	log.Printf(event)
+
+	// ctx := context.Background()
+	// ts := oauth2.StaticTokenSource(
+	// 	&oauth2.Token{AccessToken: githubToken},
+	// )
+	// tc := oauth2.NewClient(ctx, ts)
+	// client := github.NewClient(tc)
+
+	// client.Issues.CreateComment(ctx, )
+
 }
