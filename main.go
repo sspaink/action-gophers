c23bbb675a2f90fba102a9a5800af08cfb614631
Bas <3441183+BattleBas@users.noreply.github.com>
2020-03-13 21:22:16 -0500

    Use go-github pull request struct Also added new gopher gif

--- main.go
+++ main.go
@@ -13,29 +13,6 @@ import (
 	"github.com/sethvargo/go-githubactions"
 )
 
-// PullRequestEvent is the root of the event payload related to a pull request
-// More details: https://developer.github.com/v3/activity/events/types/#pullrequestevent
-type PullRequestEvent struct {
-	PullRequest `json:"pull_request"`
-	Repository  `json:"repository"`
-}
-
-// PullRequest contains the information describing the pr
-type PullRequest struct {
-	Number int
-}
-
-// Repository contains the information describing the repo the pr is beind made to
-type Repository struct {
-	Name  string
-	Owner `json:"owner"`
-}
-
-// Owner contains the information about the org or individual who created the repo
-type Owner struct {
-	Login string
-}
-
 func main() {
 
 	githubToken := githubactions.GetInput("GITHUB_TOKEN")
@@ -50,10 +27,10 @@ func main() {
 		return
 	}
 
-	var pr PullRequestEvent
+	var pr github.PullRequestEvent
 	json.Unmarshal(event, &pr)
 
-	log.Printf("Number: %d Repo Name: %s, Owner: %s", pr.PullRequest.Number, pr.Repository.Name, pr.Owner.Login)
+	log.Printf("Number: %d Repo Name: %s, Owner: %s", *pr.PullRequest.Number, *pr.Repo.Name, *pr.Repo.Owner.Login)
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: githubToken},
@@ -62,13 +39,13 @@ func main() {
 	client := github.NewClient(tc)
 
 	// TODO make and add more Gopher gifs!
-	url := "https://media.giphy.com/media/S6q921h64g8Qi305AJ/giphy.gif"
+	url := "https://media.giphy.com/media/J0CZBmDGBStZQK4nfe/giphy.gif"
 
 	message := "![GopherGif](" + url + ")"
 	comment := &github.IssueComment{
 		Body: &message,
 	}
-	_, _, err = client.Issues.CreateComment(ctx, pr.Owner.Login, pr.Repository.Name, pr.Number, comment)
+	_, _, err = client.Issues.CreateComment(ctx, *pr.Repo.Owner.Login, *pr.Repo.Name, *pr.PullRequest.Number, comment)
 	if err != nil {
 		log.Printf(err.Error())
 		return
